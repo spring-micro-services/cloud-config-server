793a17e53e908c696797e10fcd1d3f62f108afa6
ThirupathiReddy <tvajjala@gmail.com>
2017-05-06 10:37:38 -0700

    Integrated with ELK Stack for centralized loggin

--- README.md
+++ README.md
@@ -5,3 +5,20 @@ Configuration Server which manages config-repo
 Micro Services Configuration used to manage configuration globally with High availability services like github.com
 
 
+This is the configuration server to centralize the configuration properties
+ 
+ *###################################
+ *  CENTRALIZED LOG CONFIGURATION
+ *###################################
+ *  This application configured with ELK Stack for centralized logging
+ *
+ *  1. brew install logstash
+ *  2. brew install elasticsearch
+ *  3. brew install kibana
+ *  4. elasticsearch   => to start on port 9200
+ *  5. logstash -f logstash.conf on port 9600
+ *  6. kibana on port 5601
+ *
+ *###################################
+ *
+ *
--- src/main/java/com/innominds/ConfigServerApplication.java
+++ src/main/java/com/innominds/ConfigServerApplication.java
@@ -4,11 +4,30 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+//@formatter:off
 /**
  * This is the configuration server to centralize the configuration properties
  *
+ *###################################
+ *  CENTRALIZED LOG CONFIGURATION
+ *###################################
+ *  This application configured with ELK Stack for centralized logging
+ *
+ *  1. brew install logstash
+ *  2. brew install elasticsearch
+ *  3. brew install kibana
+ *  4. elasticsearch   => to start on port 9200
+ *  5. logstash -f logstash.conf on port 9600
+ *  6. kibana on port 5601
+ *
+ *###################################
+ *
+ *
+ *
+ *
  * @author ThirupathiReddy V
  */
+//@formatter:on
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
@@ -16,7 +35,6 @@ public class ConfigServerApplication {
     public static void main( String[] args ) {
 
         // SpringApplication.run( ConfigServerApplication.class, args );
-
         new SpringApplicationBuilder( ConfigServerApplication.class ).run( args );
 
     }
